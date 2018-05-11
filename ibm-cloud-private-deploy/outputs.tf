53d14292dc7c5da8ed37bd8b00d2f974e3283d8b
svermaak <shaun.vermaak@ittelligence.com>
2018-05-11 15:19:17 +0200

    Update outputs.tf

--- ibm-cloud-private-deploy/outputs.tf
+++ ibm-cloud-private-deploy/outputs.tf
@@ -13,6 +13,6 @@
 #
 ################################################################
 
-output "icp-master-vm-ip" {
-    value = "${openstack_compute_instance_v2.icp-master-vm.network.0.fixed_ip_v4}"
-}
+#output "icp-master-vm-ip" {
+#    value = "${openstack_compute_instance_v2.icp-master-vm.network.0.fixed_ip_v4}"
+#}
